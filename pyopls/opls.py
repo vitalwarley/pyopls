26c0cd28e5fa4d2918edd1c7115bdbbb691102ea
vitalwarley <vitalwarley@gmail.com>
2020-04-29 00:10:34 -0300

    Add VIP Score adapted for OPLS (4th variant)

--- pyopls/opls.py
+++ pyopls/opls.py
@@ -71,11 +71,19 @@ class OPLS(BaseEstimator, TransformerMixin):
         self.W_ortho_ = None
         self.P_ortho_ = None
         self.T_ortho_ = None
+        self.Q_ortho = None
+        self.T_ = None
+        self.P_ = None
+        self.Q_ = None
 
         self.x_mean_ = None
         self.y_mean_ = None
         self.x_std_ = None
         self.y_std_ = None
+        
+        self.vip_o_ = None
+        self.vip_p_ = None
+        self.vip_t_ = None
 
     def fit(self, X, Y):
         """Fit model to data
@@ -105,26 +113,44 @@ class OPLS(BaseEstimator, TransformerMixin):
         w = np.dot(X.T, Y)  # calculate weight vector
         w /= np.linalg.norm(w)  # normalize weight vector
 
+        T = []
+        P = []
+        Q = []
         W_ortho = []
         T_ortho = []
         P_ortho = []
+        Q_ortho = []
 
         for i in range(self.n_components):
             t = np.dot(Z, w)  # scores vector
+            q = np.dot(Y.T, t) / np.dot(t.T, t).item()  # loadings of y
+            u = np.dot(Y, q)  # scores of y
             p = np.dot(Z.T, t) / np.dot(t.T, t).item()  # loadings of X
             w_ortho = p - np.dot(w.T, p).item() / np.dot(w.T, w).item() * w  # orthogonal weight
             w_ortho = w_ortho / np.linalg.norm(w_ortho)  # normalize orthogonal weight
             t_ortho = np.dot(Z, w_ortho)  # orthogonal components
             p_ortho = np.dot(Z.T, t_ortho) / np.dot(t_ortho.T, t_ortho).item()
+            # not sure if q_ortho is OK, but it follows q
+            q_ortho = np.dot(Y.T, t_ortho) / np.dot(t_ortho.T, t_ortho).item()
             Z -= np.dot(t_ortho, p_ortho.T)
+            T.append(t)
+            P.append(p)
+            Q.append(q)
             W_ortho.append(w_ortho)
             T_ortho.append(t_ortho)
             P_ortho.append(p_ortho)
+            Q_ortho.append(q_ortho)
 
+        self.T = np.hstack(T)
+        self.P = np.hstack(P)
+        self.Q = np.hstack(Q)
         self.W_ortho_ = np.hstack(W_ortho)
         self.T_ortho_ = np.hstack(T_ortho)
         self.P_ortho_ = np.hstack(P_ortho)
-
+        self.Q_ortho_ = np.hstack(Q_ortho)
+        
+        self._vipscore()
+        
         return self
 
     def transform(self, X):
@@ -192,3 +218,49 @@ class OPLS(BaseEstimator, TransformerMixin):
         X = check_array(X)
         Z = self.transform(X)
         return np.sum(np.square(Z)) / np.sum(np.square(X - self.x_mean_))  # Z is already properly centered
+
+    
+    def _vipscore(self):
+        """VIP4 for OPLS as defined in Galindo-Prieto et al (2014).
+        
+        Inspired by `vip.m` in libPLS MATLAB library and `ropls-functions.R`.
+        """
+        T = self.T
+        P = self.P
+        Q = self.Q
+        To = self.T_ortho_
+        Po = self.P_ortho_
+        Qo = self.Q_ortho_
+
+        # sum of squares for x and y, both orthogonal and predictive
+        sxo = np.diag(np.dot(np.dot(To.T, To), np.dot(Po.T, Po)))
+        sxp = np.diag(np.dot(np.dot(T.T, T), np.dot(P.T, P)))
+        syo = np.diag(np.dot(np.dot(To.T, To), np.dot(Qo.T, Qo)))
+        syp = np.diag(np.dot(np.dot(T.T, T), np.dot(Q.T, Q)))
+
+        # sum of squares cummlative
+        sxoCum = np.sum(sxo)
+        sxpCum = np.sum(sxp)
+        ssxCum = sxpCum + sxoCum
+        syoCum = np.sum(syo)
+        sypCum = np.sum(syp)
+        ssyCum = sypCum + syoCum
+
+        # number of observations
+        K = T.shape[0]
+        ko = K / (sxoCum / ssxCum + syoCum / ssyCum)
+        kp = K / (sxpCum / ssxCum + sypCum / ssyCum)
+
+        # normalized loadings
+        PNorm = P / np.linalg.norm(P)
+        PoNorm = Po / np.linalg.norm(Po)
+
+        # vip score
+        # see equation 2.14 in Galinto-Prieto (2017)
+        self.vip_o_ = np.sqrt(ko * (np.dot(np.square(PoNorm), sxo) / ssxCum + (np.dot(np.square(PoNorm), syo) / ssyCum)))
+        self.vip_p_ = np.sqrt(kp * (np.dot(np.square(PNorm), sxp) / ssxCum + (np.dot(np.square(PNorm), syp) / ssyCum)))
+        self.vip_t_ = np.sqrt(K/2 * (np.dot(np.square(PoNorm), sxo) / ssxCum + 
+                                     np.dot(np.square(PNorm), sxp) / ssxCum + 
+                                     np.dot(np.square(PoNorm), syo) / ssyCum +
+                                     np.dot(np.square(PNorm), syp) / ssyCum))
+        
